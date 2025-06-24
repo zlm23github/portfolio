c3ad929e6ec65397493d525888bf9b3e2227f9f5
liming <zlm17unl@gmail.com>
2025-06-24 12:25:12 -0400

    update Router method

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import About from './pages/About'
 import Project from './pages/Project'
 import Contact from './pages/Contact'
